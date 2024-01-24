c55bae052f3acecf08f7e4330e6d357296e67082
izruff <rafaelky.dev@gmail.com>
2024-01-25 01:32:26 +0800

    Added more to post create page

--- src/pages/PostCreatePage.tsx
+++ src/pages/PostCreatePage.tsx
@@ -1,25 +1,62 @@
-import { Box, Chip, Stack, TextField, Typography } from "@mui/material";
+import { Box, Button, Card, Chip, Divider, InputBase, Stack, TextField, Typography } from "@mui/material";
 import React from "react";
 
 const PostCreatePage = () => {
-  const [topic, setTopic] = React.useState("");
-  const [tags, setTags] = React.useState([]);
+  const [postTitle, setPostTitle] = React.useState("");
+  const [postContent, setPostContent] = React.useState("");
+  const [postTopic, setPostTopic] = React.useState("");
+  const [postTags, setPostTags] = React.useState(["some", "tags", "being", "shown"]);
+
+  function handleTitleChange(event) {
+    setPostTitle(event.target.value);
+  }
+
+  function handleContentChange(event) {
+    setPostContent(event.target.value)
+  }
+
+  function handleTagDelete(tagToDelete) {
+    setPostTags((postTags) => postTags.filter((tag) => tag !== tagToDelete));
+  }
+
+  function handleCreatePost(event) {
+    
+  }
 
   return (
     <>
-      <Typography variant="h2">Create Post</Typography>
-      <Stack direction="row" justifyContent="space-between">
-        <TextField id="topic" label="Topic" />
-        <TextField id="tags" label="Tags" />
-        <TextField id="title" label="Title" />
+      <Typography variant="h2" marginBottom={4}>Create Post</Typography>
+      <Stack spacing={2}>
+        <Box>
+          <Chip label={postTopic} variant="outlined" />
+          {postTags.map((tag) => (
+            <Chip label={tag} variant="filled" onDelete={() => handleTagDelete(this.label)} /> // TODO: how to reference the label?
+          ))}
+        </Box>
+        <Card variant="outlined">
+          <Box p={2}>
+            <InputBase
+              fullWidth
+              placeholder="Title"
+              value={postTitle}
+              onChange={handleTitleChange}
+            />
+          </Box>
+          <Divider />
+          <Box p={2}>
+            <InputBase // TODO: current implementation cannot have multiple lines
+              fullWidth
+              placeholder="Insert your post contents here..."
+              value={postContent}
+              onChange={handleContentChange}
+            />
+          </Box>
+        </Card>
+        <Stack direction="row" spacing={2} justifyContent="flex-end">
+          <Button>Save Draft</Button>
+          <Button variant="contained" onClick={handleCreatePost}>Create</Button>
+        </Stack>
       </Stack>
-      <Box>
-        <Chip label="Topic: lorem-ipsum" variant="outlined" />
-        <Chip label="some" variant="filled" />
-        <Chip label="tags" variant="filled" />
-        <Chip label="being" variant="filled" />
-        <Chip label="shown" variant="filled" />
-      </Box>
     </>
   );
 };
