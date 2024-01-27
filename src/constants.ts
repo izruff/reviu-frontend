475f51b1259712ff7003a349f6fa3f8f08b8c7da
izruff <rafaelky.dev@gmail.com>
2024-01-27 11:49:43 +0800

    Fixed API URL env variable
    I forgot to do this earlier. There are some adjustments to be made in the vase of Vite; refer to [this documentation](https://vitejs.dev/guide/env-and-mode).

--- src/constants.ts
+++ src/constants.ts
@@ -1,4 +1,3 @@
-// export const API_URL = process.env.API_KEY ?? "localhost:8080";
-export const API_URL = "http://localhost:8080";
+export const API_URL = import.meta.env.VITE_API_URL || "http://localhost:8080";
 
 export const HUBS = ["books", "movies", "series", "anime", "gaming"];
