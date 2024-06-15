77d220512f1a6f5cb863fbcf72e800fc454de803
izruff <rafaelky.dev@gmail.com>
2024-06-15 17:38:56 +0800

    style: Prettier formatting

--- src/main.tsx
+++ src/main.tsx
@@ -2,7 +2,7 @@ import App from "./app/App.tsx";
 import { store } from "./app/store.ts";
 import React from "react";
 import ReactDOM from "react-dom/client";
-import { Provider } from 'react-redux'
+import { Provider } from "react-redux";
 
 ReactDOM.createRoot(document.getElementById("root")!).render(
   <React.StrictMode>
